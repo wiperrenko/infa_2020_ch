4e0e6551432e4d9cb155428f60435de686f604d7
wiperrenko <72004775+wiperrenko@users.noreply.github.com>
2021-02-11 10:55:14 +0300

    Add files via upload

---
+++ lab 2/pipki.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int a, sum = 0;
+    cin >> a;
+    while (a > 0)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    cout << sum;
+    return 0;
+}
+
+
