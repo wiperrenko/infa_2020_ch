a5fdebb433cc5872760eac5efe94400a27b94a91
wiperrenko <72004775+wiperrenko@users.noreply.github.com>
2021-02-11 10:52:16 +0300

    Add files via upload

---
+++ lab 2/pipki.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (b < c)
+        swap(b, c);
+    if (a < b)
+        swap(a, b);
+    if (a < b + c)
+        cout << "impossible";
+    else
+    {
+        if (a * a == b * b + c * c)
+            cout << "right";
+        if (a * a < b * b + c * c)
+            cout << "acute";
+        if (a * a > b * b + c * c)
+            cout << "obtuse";
+    }
+    return 0;
+}
+
+
